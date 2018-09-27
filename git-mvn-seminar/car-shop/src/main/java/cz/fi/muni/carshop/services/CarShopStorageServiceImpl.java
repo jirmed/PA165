c6e79357da19f989fb14d26b482e6558116318f0
jirmed <jirka@medvidci.cz>
2018-09-27 12:24:28 +0200

    Added method sellCar

--- git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
+++ git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
@@ -10,27 +10,38 @@ import java.util.stream.Collectors;
 import cz.fi.muni.carshop.CarShopStorage;
 import cz.fi.muni.carshop.entities.Car;
 import cz.fi.muni.carshop.enums.CarTypes;
+import cz.fi.muni.carshop.exceptions.RequestedCarNotFoundException;
 
 public class CarShopStorageServiceImpl implements CarShopStorageService {
 
-	@Override
-	public Optional<Car> isCarAvailable(Color color, CarTypes type) {
-		Map<CarTypes, List<Car>> allCars = CarShopStorage.getInstancce().getCars();
-		List<Car> carsOfSameType = allCars.get(type);
-		return carsOfSameType.stream().filter(car -> car.getColor().equals(color)).findAny();
-	}
+    @Override
+    public Optional<Car> isCarAvailable(Color color, CarTypes type) {
+        Map<CarTypes, List<Car>> allCars = CarShopStorage.getInstancce().getCars();
+        List<Car> carsOfSameType = allCars.get(type);
+        return carsOfSameType.stream().filter(car -> car.getColor().equals(color)).findAny();
+    }
 
-	@Override
-	public List<Car> getCheaperCarsOfSameTypeAndYear(Car referenceCar) {
-		Map<CarTypes, List<Car>> allCars = CarShopStorage.getInstancce().getCars();
-		List<Car> carsOfSameType = allCars.get(referenceCar.getType());
-		return carsOfSameType.stream().filter(car -> referenceCar.getConstructionYear() == car.getConstructionYear()
-				&& car.getPrice() < referenceCar.getPrice()).collect(Collectors.toList());
-	}
+    @Override
+    public List<Car> getCheaperCarsOfSameTypeAndYear(Car referenceCar) {
+        Map<CarTypes, List<Car>> allCars = CarShopStorage.getInstancce().getCars();
+        List<Car> carsOfSameType = allCars.get(referenceCar.getType());
+        return carsOfSameType.stream().filter(car -> referenceCar.getConstructionYear() == car.getConstructionYear()
+                && car.getPrice() < referenceCar.getPrice()).collect(Collectors.toList());
+    }
 
-	@Override
-	public void addCarToStorage(Car car) {
-		CarShopStorage.getInstancce().getCars().computeIfAbsent(car.getType(), x -> new ArrayList<>()).add(car);
-	}
+    @Override
+    public void addCarToStorage(Car car) {
+        if (car.getPrice() < 0) {
+            throw new IllegalArgumentException("Negative price is not allowed");
+        }
+        CarShopStorage.getInstancce().getCars().computeIfAbsent(car.getType(), x -> new ArrayList<>()).add(car);
+    }
+
+    @Override
+    public void sellCar(Car car) throws RequestedCarNotFoundException {
+        if (CarShopStorage.getInstancce().getCars().remove(car.getType()).isEmpty()) {
+            throw new RequestedCarNotFoundException(car.toString());
+        }
+    }
 
 }
