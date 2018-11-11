f2990de16b7c61dae990c864a031122df5978d09
Jiri Medved <medved@volny.cz>
2018-11-11 18:00:57 +0100

    Task 02

--- eshop-ws/src/main/java/cz/fi/muni/pa165/ws/endpoints/ProductEndPoint.java
+++ eshop-ws/src/main/java/cz/fi/muni/pa165/ws/endpoints/ProductEndPoint.java
@@ -2,6 +2,7 @@ package cz.fi.muni.pa165.ws.endpoints;
 
 import cz.fi.muni.pa165.ws.entities.products.GetProductRequestByName;
 import cz.fi.muni.pa165.ws.entities.products.GetProductResponse;
+import cz.fi.muni.pa165.ws.entities.products.GetProductsRequest;
 import cz.fi.muni.pa165.ws.entities.products.Product;
 import cz.fi.muni.pa165.ws.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,43 +15,50 @@ import java.util.List;
 /**
  * This class represents an endpoint for the Product class generated from the
  * XSD definitions.
- * 
+ *
  * @author brossi
  */
 @Endpoint
 public class ProductEndPoint {
- 
+
     private static final String NAMESPACE_URI = "http://muni.fi.cz/pa165/ws/entities/products";
 
     @Autowired
     private ProductRepository productRepository;
 
-    
-        /**
-         * 
-         *  Payload root defines an handler method, by using the general base namespace uri
-         *  and the specific part for this handler
-         *  we mark to return the ResponsePayload and we annotate the variable for the 
-         *  RequestPayload
-         * 
-         * See http://docs.spring.io/spring-ws/site/apidocs/org/springframework/ws/server/endpoint/annotation/PayloadRoot.html
-         * 
-         */
-	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getProductRequestByName")
-	@ResponsePayload
-	public GetProductResponse getProduct(@RequestPayload GetProductRequestByName request) {
-		
-            final GetProductResponse response = new GetProductResponse();
-            final Product product = productRepository.getProductByName(request.getName());
+    /**
+     *
+     * Payload root defines an handler method, by using the general base
+     * namespace uri and the specific part for this handler we mark to return
+     * the ResponsePayload and we annotate the variable for the RequestPayload
+     *
+     * See
+     * http://docs.spring.io/spring-ws/site/apidocs/org/springframework/ws/server/endpoint/annotation/PayloadRoot.html
+     *
+     */
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getProductRequestByName")
+    @ResponsePayload
+    public GetProductResponse getProduct(@RequestPayload GetProductRequestByName request) {
 
-            
-            response.setProduct(product);
-            return response;
-            
-	}
-        
-        
-        // TODO: Create here a method to return all Products, getProducts() or getAllProducts() with signature List<Product>
-      
-    
+        final GetProductResponse response = new GetProductResponse();
+        final Product product = productRepository.getProductByName(request.getName());
+
+        //response.setProduct(product);
+        response.getProduct().add(product);
+        return response;
+
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getProductsRequest")
+    @ResponsePayload
+    public GetProductResponse getProducts(@RequestPayload GetProductsRequest request) {
+
+        final GetProductResponse response = new GetProductResponse();
+        final List<Product> products = productRepository.getProducts();
+        products.forEach((product) -> {
+            response.getProduct().add(product);
+        });
+        return response;
+    }
+
 }
